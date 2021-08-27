a09f6cd9bdced187036b938180bab000f1203e0b
Juana Paulina Aguila Hernandez <vegenisennawa@users.noreply.github.com>
2021-08-26 22:14:01 -0500

    Go BootCAMP REST API
    Exercise to use to aply to BootCAMP.

---
+++ GO_BootcampRestAPI/Controllers/HelloFriendController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GO_BootcampRestAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloFriendController : ControllerBase
+    {
+        private readonly ILogger<HelloFriendController> _logger;
+
+        public HelloFriendController(ILogger<HelloFriendController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public HelloFriend get()
+        {
+            HelloFriend message = new HelloFriend();
+            message.HelloWorldMessage = "Hello world";
+            return message;
+        }
+    }
+}
---
+++ GO_BootcampRestAPI/HelloFriend.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GO_BootcampRestAPI
+{
+    public class HelloFriend
+    {
+        public string HelloWorldMessage { get; set; }
+    }
+}
